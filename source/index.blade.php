f28fc9511e5322955fbdcfe7306d68d688e69ffe
Nuruzzaman Milon <contact@milon.im>
2023-09-11 15:47:37 -0400

    update index file

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,12 +8,10 @@
 
             <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
 
-            <p class="text-lg">Give your documentation a boost with Jigsaw. <br class="hidden sm:block">Generate elegant, static docs quickly and easily.</p>
+            <p class="text-lg">Unlock the secrets to acing system design interviews with our comprehensive course. Master the art of designing scalable and efficient systems to not only secure your dream job in tech but also have real world insight of how scalable systems do actually wokrs!</p>
 
             <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
-
-                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
+                <a href="/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
             </div>
         </div>
 
@@ -26,23 +24,23 @@
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
 
-            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Templating with <br>Laravel's Blade engine</h3>
+            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Get introduction to real world system components.</h3>
 
-            <p>Blade is a powerful, simple, and beautiful templating language, and now you can use it for your static sites, not just your Laravel-powered apps.</p>
+            <p>Explore the intricacies of real-world system components and gain practical insights that will empower you to excel in system design interviews and tackle complex engineering challenges.</p>
         </div>
 
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
 
-            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Use Markdown for <br>content-driven pages</h3>
+            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Example of real world system</h3>
 
-            <p>Markdown is the web’s leading format for writing articles, blog posts, documentation, and more. Jigsaw makes it painless to work with Markdown content.</p>
+            <p>Dive into real-world system examples, dissecting the architecture of industry-leading applications and platforms to sharpen your system design skills for today's tech landscape.</p>
         </div>
 
         <div class="mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
 
-            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Compile your assets <br>using Laravel Mix </h3>
+            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Explanation of all the system design termonology</h3>
 
             <p>Jigsaw comes pre-configured with Laravel Mix, a simple and powerful build tool. Use the latest frontend tech with just a few lines of code.</p>
         </div>
