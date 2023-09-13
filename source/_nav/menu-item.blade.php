96a34c9cb85bcc67fe5988347734239955ee6e3c
Nuruzzaman Milon <contact@milon.im>
2023-09-12 22:30:50 -0400

    update navbar

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -2,13 +2,13 @@
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
-            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-500' : '' }} nav-menu__item hover:text-blue-500"
+            class="nav-menu__item {{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl'.$level.'-active' : '' }} {{ $page->isActive($url) ? 'lvl'.$level.'-active active text-blue-500' : '' }} hover:text-blue-500"
         >
             {{ $label }}
         </a>
     @else
         {{-- Menu item without URL--}}
-        <p class="nav-menu__item text-gray-600">{{ $label }}</p>
+        <p class="nav-menu__item {{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} hover:text-blue-500 ">{{ $label }}</p>
     @endif
 
     @if (! is_string($item) && $item->children)
